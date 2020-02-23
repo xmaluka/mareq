3adf2c7c2f216bac84b630161052030afec22aac
Marcio Maluka <marciomaluka@ymail.com>
2020-02-23 14:21:44 +0200

    added dictionary

--- marcio_module/mar_eq.py
+++ marcio_module/mar_eq.py
@@ -1,1 +1,10 @@
-# Python file for the equations
+import numpy as np
+import pandas as pd
+
+# Dictionary which  gives sample mean, standard deviation, variance and other staticstics:
+def data_summary(list1):
+    data_summary={'mean':round(np.mean(list1),2),
+                  'median':round(np.percentile(list1,50),2),
+                  'min':round(min(list1),2),
+                  'max':round(max(list1),2),
+                  'std':round(np.std(list1,ddof=1),2)}
